eabdfbd9aad0909bfa053f68b4843f20d6d44271
huangyu <598050554@qq.com>
2021-02-22 20:03:28 +0800

    add icon, circle, marker

--- src/components/Map/index.vue
+++ src/components/Map/index.vue
@@ -6,8 +6,6 @@
 import 'leaflet/dist/leaflet.css'
 import 'leaflet'
 
-const icon = require('./mapIcon/iconSetting')
-
 export default {
   name: 'Map',
   data() {
@@ -20,33 +18,87 @@ export default {
     this.initMap()
   },
   created() {
-    console.log(icon)
   },
   methods: {
     initMap() {
-      delete L.Icon.Default.prototype._getIconUrl
-      L.Icon.Default.mergeOptions({
-        iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-        iconUrl: require("leaflet/dist/images/marker-icon.png"),
-        shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-      })
-
       var map = L.map(
         'map-collection',
       {
         // 移除右下角 leaflet 标识链接
         attributionControl: false,
       })
-        .setView([39.77287, 110.1897], 13)
+        .setView([30.471258, 104.011849], 13)
 
       L.tileLayer(
         this.url,
       ).addTo(map)
 
-      var marker = L.marker(
-        [39.77287, 110.1897],
-      ).addTo(map).bindPopup('I am a Popup');
+      var iconCd = L.icon({
+        iconUrl:      require('@/assets/images/icon/leaf-green.png'),
+        shadowUrl:    require('@/assets/images/icon/leaf-shadow.png'),
+
+        iconSize:     [38, 95],   // size of the icon
+        shadowSize:   [50, 64],   // size of the shadow
+        iconAnchor:   [22, 94],   // 对应于标记位置的图标点
+        shadowAnchor: [4, 62],    // the same for the shadow
+        popupAnchor:  [-3, -76]   // 相对于图标锚，弹出框应该从哪个点打开
+      })
+
+      var iconWz = L.icon({
+        iconUrl:      require('@/assets/images/icon/leaf-red.png'),
+        shadowUrl:    require('@/assets/images/icon/leaf-shadow.png'),
+
+        iconSize:     [38, 95],   // size of the icon
+        shadowSize:   [50, 64],   // size of the shadow
+        iconAnchor:   [22, 94],   // 对应于标记位置的图标点
+        shadowAnchor: [4, 62],    // the same for the shadow
+        popupAnchor:  [-3, -76]   // 相对于图标锚，弹出框应该从哪个点打开
+      })
+
+      var markerCd = L.marker(
+        [30.471258, 104.011849],{
+          icon: iconCd
+        }
+      ).addTo(map).bindPopup('I am in ChenDu')
+
+      var markerWz = L.marker(
+        [27.809299, 120.520408],
+        {
+          icon: iconWz
+        }
+      ).addTo(map).bindPopup('You am in WenZhou').openPopup()
+
+      var polygon = L.polygon([
+        [30.471258, 104.011849],
+        [27.809299, 120.520408]
+      ], {
+        color: '#F8BBD0',
+      }).addTo(map).bindPopup()
+
+      var greenCircle = L.circle(
+        [30.471258, 104.011849],
+        {
+          color: '#B9F6CA',
+          fillColor: '#81C784',
+          fillOpacity: 0.5,
+          radius: 50000
+      }).addTo(map)
+
+      var pinkCircle = L.circle(
+        [27.809299, 120.520408],
+        {
+          color: '#FF80AB',
+          fillColor: '#F48FB1',
+          fillOpacity: 0.5,
+          radius: 50000
+        }).addTo(map)
+
+      // map.on('click', this.onMapClick())
+
       this.map = map
+    },
+    onMapClick(e) {
+      alert("You clicked the map at")
     }
   }
 }
